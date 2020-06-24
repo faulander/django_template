a5629c322dbd1ae7aa64f7726edc8c7a94bc1daf
faulander <harald.fauland@gmail.com>
2020-06-24 17:42:10 +0200

    added ALLAUTH and a superuser account

--- startapp.prod.sh
+++ startapp.prod.sh
@@ -2,4 +2,5 @@
 
 docker-compose -f docker-compose.prod.yml up -d --build
 docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
+docker-compose -f docker-compose.prod.yml exec web python manage.py loaddata create_superuser
 docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
